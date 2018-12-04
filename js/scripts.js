624a08f6c6248f27ce959fb79493d8fabd863f9f
Andrea Brucculeri <andrea@brucculeri.com>
2018-12-03 23:12:28 -0500

    finally making another commit because I fixed my embarrassing disaster

--- js/scripts.js
+++ js/scripts.js
@@ -24,53 +24,6 @@ $(function(){
   var userIng1 = '';
   var userIng2 = '';
   var btn = document.getElementById('submit');
-  var foodbtn = document.getElementById('search');
-  var getFood = document.getElementById('food-search');
-  var findFood = '';
-
-  /*foodbtn.addEventListener('click', function(){
-    var url= "";
-    var data= [];
-    var html= '';
-    findFood = getFood.value;
-    console.log(findFood);
-
-    $.ajax({
-      url: "https://data.mo.gov/resource/ik3n-3nbb.json",
-      type: "GET",
-      data: {
-        "$limit" : 5000,
-        "$$app_token" : foodToken
-      },
-      success:function(data){
-        var state = data.map(data => data.location_state);
-        var address = data.map(data => data.location_address);
-        var city = data.map(data => data.location_city);
-        var zip = data.map(data => data.location_zip);
-        var agency = data.map(data => data.agency_name);
-        var county = data.map(data => data.county);
-        var phone = data.map(data => data.phone_number);
-
-        var tablehtml ='';
-        var tableContent = document.getElementById('table-content');
-        function buildTable(){
-          tablehtml += '<tr><th>Agency</th><th>Phone Number</th><th>Address</th></tr>';
-          for (i=0; i < data.length; i++){
-          //  if(findFood === city[i] || state[i] || zip[i]){
-          if(findFood === city[i] || findFood === state[i] ||findFood === zip[i] ){
-              tablehtml += '<tr>';
-              tablehtml += '<td>' + agency[i] + '</td><td>' + phone[i] + '</td><td>' + address[i] + city[i] + state[i] + zip[i] + '</td>';
-              tablehtml += '</tr>';
-            } // close if
-          }  // close forloop
-          tableContent.innerHTML = tablehtml;
-        } // close buildtable
-        buildTable();
-
-      } // success function
-    }); // close ajax
-  }); // close button */
-
 
   btn.addEventListener('click', function(){
     userIng1 = userFood1.value;
@@ -119,5 +72,120 @@ $(function(){
     }); // ajax
 
   }); // close button
+
+
+  Highcharts.chart('container', {
+  chart: {
+    style: {
+        fontFamily: 'times'
+    },
+    type: 'column'
+  },
+  title: {
+    text: 'Weight Percentages of Food Loss and Waste (in percentage of what enters each step)'
+  },
+  xAxis: {
+    categories: ['Agricultural Production', 'Postharvest handling and storage', 'Processing and packaging', 'Distribution', 'Consumption']
+  },
+  yAxis: {
+    min: 0,
+    ceiling: 160,
+    title: {
+      text: 'Percentage Waste'
+    },
+    stackLabels: {
+      enabled: true,
+      style: {
+        fontWeight: 'bold',
+        fontFamily: 'times',
+        color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+      }
+    }
+  },
+  legend: {
+    align: 'center',
+    x: -10,
+    verticalAlign: 'top',
+    y: 25,
+    floating: true,
+    backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+    borderColor: '#CCC',
+    borderWidth: 1,
+    shadow: false
+  },
+  tooltip: {
+    headerFormat: '<b>{point.x}</b><br/>',
+    pointFormat: '{series.name}: {point.y} percent <br/>Total Waste: {point.stackTotal} percent'
+  },
+  plotOptions: {
+    column: {
+      stacking: 'normal',
+      dataLabels: {
+        enabled: true,
+        color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+      }
+    }
+  },
+  series: [{
+    name: 'Cereals',
+    data: [2, 2, 5.5, 2, 27]
+  }, {
+    name: 'Roots and tubers',
+    data: [20, 10, 15, 7, 30]
+  },
+  {
+    name: 'Oilseeds and pulses',
+    data: [12, 0, 5, 1, 4]
+  },
+  {
+    name: 'Fruits and vegetables',
+    data: [20, 4, 2, 12, 28]
+  },{
+    name: 'Meat',
+    data: [3.5, 1, 5, 4, 11]
+  }, {
+    name: 'Fish and seafood',
+    data: [12, .5, 6, 9, 33]
+  },  {
+    name: 'Milk',
+    data: [3.5, 0.5, 1.2, 0.5, 15]
+  }],
+  responsive: {
+    rules: [{
+        condition: {
+            maxWidth: 500
+        },
+        chartOptions: {
+            legend: {
+                align: 'center',
+                verticalAlign: 'top',
+                y: 85,
+                layout: 'horizontal'
+            },
+            yAxis: {
+                labels: {
+                    align: 'left',
+                    x: 0,
+                    y: -5
+                },
+                title: {
+                    text: null
+                }
+            },
+            xAxis: {
+              labels: {
+                autoRotation: [-90]
+              }
+            },
+            subtitle: {
+                text: null
+            },
+            credits: {
+                enabled: false
+            }
+        }
+    }]
+}
+}); // close highchart
 
 }); // close wrapper
